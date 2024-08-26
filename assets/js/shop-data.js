cf1056ef856f62dbc74cce4763266607fda95a5a
Mackedonec <mackedonec187@gmail.com>
2024-08-26 15:54:59 +0300

    save

--- assets/js/shop-data.js
+++ assets/js/shop-data.js
@@ -19,6 +19,86 @@ const shopCards = [
     color: "Якийсь колір 1",
     stock: 100,
   },
+  {
+    id: 2,
+    img: "assets/img/tamplate.png",
+    dataSearchName: "search-names",
+    searchName: "Гарний товар",
+    dataPrice: "price",
+    price: "7500",
+    cardDescript: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+    dataBrand: "brand-2",
+    brand: "Виробник 2",
+    dataSeries: "series-2",
+    series: "Якась серія 2",
+    dataModel: "model-2",
+    model: "Якась модель 2",
+    dataCountry: "country-2",
+    country: "Якась країна 2",
+    dataColor: "color-2",
+    color: "Якийсь колір 2",
+    stock: 50,
+  },
+  {
+    id: 3,
+    img: "assets/img/tamplate.png",
+    dataSearchName: "search-names",
+    searchName: "Дуже якісний товар",
+    dataPrice: "price",
+    price: "11000",
+    cardDescript: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+    dataBrand: "brand-1",
+    brand: "Виробник 1",
+    dataSeries: "series-3",
+    series: "Якась серія 3",
+    dataModel: "model-2",
+    model: "Якась модель 2",
+    dataCountry: "country-3",
+    country: "Якась країна 3",
+    dataColor: "color-3",
+    color: "Якийсь колір 3",
+    stock: 50,
+  },
+  {
+    id: 4,
+    img: "assets/img/tamplate.png",
+    dataSearchName: "search-names",
+    searchName: "Дорогий та якісний товар",
+    dataPrice: "price",
+    price: "18000",
+    cardDescript: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+    dataBrand: "brand-4",
+    brand: "Виробник 4",
+    dataSeries: "series-1",
+    series: "Якась серія 1",
+    dataModel: "model-2",
+    model: "Якась модель 2",
+    dataCountry: "country-3",
+    country: "Якась країна 3",
+    dataColor: "color-others",
+    color: "Інший колір",
+    stock: 10,
+  },
+  {
+    id: 5,
+    img: "assets/img/tamplate.png",
+    dataSearchName: "search-names",
+    searchName: "Дешевий товар",
+    dataPrice: "price",
+    price: "5000",
+    cardDescript: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+    dataBrand: "brand-4",
+    brand: "Виробник 4",
+    dataSeries: "series-1",
+    series: "Якась серія 1",
+    dataModel: "model-6",
+    model: "Якась модель 6",
+    dataCountry: "country-2",
+    country: "Якась країна 2",
+    dataColor: "color-2",
+    color: "Якийсь колір 2",
+    stock: 0,
+  },
 ].map((card) => {
   if (card.stock > 10) {
     card.dataStock = "in-stock";
@@ -144,6 +224,8 @@ function filterCards(
   minPrice,
   maxPrice,
   availabilityFilters,
+  brandFilters,
+  seriesFilters,
   cards
 ) {
   return cards.filter((card) => {
@@ -158,7 +240,21 @@ function filterCards(
       availabilityFilters.length === 0 ||
       availabilityFilters.includes(dataStock);
 
-    return matchesName && matchesPrice && matchesAvailability;
+    const dataBrand = card.dataBrand;
+    const matchesBrand =
+      brandFilters.length === 0 || brandFilters.includes(dataBrand);
+
+    const dataSeries = card.dataSeries;
+    const matchesSeries =
+      seriesFilters.length === 0 || seriesFilters.includes(dataSeries);
+
+    return (
+      matchesName &&
+      matchesPrice &&
+      matchesAvailability &&
+      matchesBrand &&
+      matchesSeries
+    );
   });
 }
 
@@ -172,13 +268,23 @@ const searchInput = document.querySelector(".main-search");
 const minPriceInput = document.querySelector(".input-min");
 const maxPriceInput = document.querySelector(".input-max");
 const rangeInputs = document.querySelectorAll(".range-input input");
-const availabilityCheckboxes = document.querySelectorAll(".search-item");
+const availabilityCheckboxes = document.querySelectorAll(
+  ".search-item[data-stock]"
+);
+const brandCheckboxes = document.querySelectorAll(".search-item[data-brand]");
+const seriesCheckboxes = document.querySelectorAll(".search-item[data-series]");
 
 searchInput.addEventListener("input", applyFilters);
 [minPriceInput, maxPriceInput, ...rangeInputs].forEach((input) => {
   input.addEventListener("input", applyFilters);
 });
 availabilityCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", applyFilters);
+});
+brandCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", applyFilters);
+});
+seriesCheckboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", applyFilters);
 });
 
@@ -191,11 +297,21 @@ function applyFilters() {
     .filter((checkbox) => checkbox.checked)
     .map((checkbox) => checkbox.dataset.stock);
 
+  const brandFilters = Array.from(brandCheckboxes)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.dataset.brand);
+
+  const seriesFilters = Array.from(seriesCheckboxes)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.dataset.series);
+
   const filteredCards = filterCards(
     searchQuery,
     minPrice,
     maxPrice,
     availabilityFilters,
+    brandFilters,
+    seriesFilters,
     shopCards
   );
   updateCardsDisplay(filteredCards);
@@ -235,12 +351,14 @@ const resetButton = document.querySelector(".search-reset");
 
 resetButton.addEventListener("click", function () {
   searchInput.value = "";
-  minPriceInput.value = 6000; // Default value
-  maxPriceInput.value = 19000; // Default value
-  rangeInputs[0].value = 6000; // Default value
-  rangeInputs[1].value = 19000; // Default value
+  minPriceInput.value = 6000;
+  maxPriceInput.value = 19000;
+  rangeInputs[0].value = 6000;
+  rangeInputs[1].value = 19000;
 
   availabilityCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+  brandCheckboxes.forEach((checkbox) => (checkbox.checked = false));
+  seriesCheckboxes.forEach((checkbox) => (checkbox.checked = false));
 
   const cardsBox = document.querySelector(".cards-box");
   const cardsHeight = document.querySelector(".hiden-cards-box");
@@ -252,5 +370,3 @@ resetButton.addEventListener("click", function () {
 
   updateCardsDisplay(shopCards);
 });
-
-localStorage.setItem("shopCards", JSON.stringify(shopCards));
